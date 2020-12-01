3ddb06b48d7e0f2d7223cd32ba4ff6759e2301fb
dimar064 <dimar064@gmail.com>
2020-12-01 20:14:43 +0200

    complete day2 tasks

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,19 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if (a <= 0) {
+            System.out.println("Ошибка ввода");
+        } else if (a < 5) {
+            System.out.println("Малоэтажный дом");
+        } else if (a < 9) {
+            System.out.println("Среднеэтажный дом");
+        } else {
+            System.out.println("Многоэтажный дом");
+        }
     }
 }
