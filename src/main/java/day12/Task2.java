cb5209c7db2d01e01ba25de8f6f92f312749f49b
dimar064 <dimar064@gmail.com>
2020-12-12 03:57:42 +0200

    task2 day12

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -6,14 +6,14 @@ import java.util.List;
 public class Task2 {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 30; i++) {
+        int i = 0;
+        while (i < 350) {
             if (i % 2 == 0) {
                 list.add(i);
             }
-        }
-        for (int i = 300; i < 350; i++) {
-            if (i % 2 == 0) {
-                list.add(i);
+            i++;
+            if (i == 30) {
+                i = 300;
             }
         }
         System.out.println(list);
