495fa32e7f971732cea1d7c9dc48d6c65d074176
dimar064 <dimar064@gmail.com>
2020-12-15 21:21:57 +0200

    complete day15 tasks

--- src/main/java/day15/task1/Task1.java
+++ src/main/java/day15/task1/Task1.java
@@ -1,7 +1,28 @@
 package day15.task1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        File file1 = new File("src/main/resources/shoes.csv");
+        File file2 = new File("src/main/resources/missing_shoes.txt");
+        try {
+            Scanner scanner = new Scanner(file1);
+            PrintWriter writer = new PrintWriter(file2);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] data = line.split(";");
+                if (data[2].equals("0")) {
+                    writer.println(line);
+                }
+            }
+            scanner.close();
+            writer.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файлы не найдены");
+        }
     }
 }
