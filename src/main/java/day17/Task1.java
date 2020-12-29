3f8fcc416fb5e44576f9e05dce1bbb0e5ff90fb6
dimar064 <dimar064@gmail.com>
2020-12-29 22:38:54 +0200

    complete day17 tasks

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -2,6 +2,12 @@ package day17;
 
 public class Task1 {
     public static void main(String[] args) {
+        ChessPiece[] chessPieces = {
+                ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE,
+                ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK};
 
+        for (ChessPiece chessPiece : chessPieces) {
+            System.out.print(chessPiece.getRepresentation() + " ");
+        }
     }
 }
